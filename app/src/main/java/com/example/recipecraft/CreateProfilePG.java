def2dffbcafe8357a1cfaa80b53e96c6e86060ac
aditidosi03 <aditidosi03@gmail.com>
2019-07-17 15:48:33 -0400

    Hello

---
+++ app/src/main/java/com/example/recipecraft/CreateProfilePG.java
@@ -0,0 +1,14 @@
+package com.example.recipecraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateProfilePG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile_pg);
+    }
+}
