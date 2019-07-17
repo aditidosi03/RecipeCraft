54a3477d50a1447a6a371f2463952a6a3c7aff55
aditidosi03 <aditidosi03@gmail.com>
2019-07-17 19:13:11 -0400

    Hello

---
+++ app/src/main/java/com/example/recipecraft/AllRecipesPG.java
@@ -0,0 +1,14 @@
+package com.example.recipecraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllRecipesPG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_recipes_pg);
+    }
+}
